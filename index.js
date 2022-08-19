e768a86e53c28e17d7ecefb6bf6ac7f8d40ac93f
andhikaa4 <110803805+andhikaa4@users.noreply.github.com>
2022-08-19 08:13:03 +0700

    day7

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+
+const app = express()
+const port = 8000
+
+app.set('view engine', 'hbs')
+app.use('/assets', express.static(__dirname + '/assets'))
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', function(req, res){
+    res.render('index')
+})
+
+
+
+app.get('/Project', function(req, res){
+    res.render('Project')
+})
+
+app.get('/contact', function(req, res){
+    res.render('contact')
+})
+
+app.post('/Project', function(req, res){
+    let title = req.body.projectTitle
+    let start = req.body.startDate
+    let end = req.body.endDate
+    let nodeJs = req.body.tech1
+    let reactJs = req.body.tech2
+    let nextJs = req.body.tech3
+    let typescript = req.body.tech4
+    let content = req.body.pDesc
+
+    if(nodeJs){
+        nodeJs = req.body.tech1
+    } else {
+        nodeJs = ''
+    }
+    if(nextJs){
+        nextJs = req.body.tech3
+    } else {
+        nextJs = ''
+    }
+    if(reactJs){
+        reactJs = req.body.tech2
+    } else {
+        reactJs = ''
+    }
+    if(typescript){
+        typescript = req.body.tech4
+    } else {
+        typescript = ''
+    }
+
+    console.log(title);
+    console.log(start);
+    console.log(end);
+    console.log(nodeJs);
+    console.log(reactJs);
+    console.log(nextJs);
+    console.log(typescript);
+    console.log(content);
+
+    res.redirect('/')
+
+})
+
+
+app.listen(port, function(){
+    console.log(`server running on port ${port}`);
+})
